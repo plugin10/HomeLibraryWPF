6d2bfbd6b99a7927abd593db06ba74a056564d0e
plugin10 <lukiwrzs@o2.pl>
2023-03-19 20:52:46 +0100

    ButtonsAddAction

---
+++ HomeLibrary/HomeLibrary/Content/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using HomeLibrary.Core;
+using System;
+
+namespace HomeLibrary.Content.ViewModels
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand ShowAvailableBooksCommand { get; set; }
+        public RelayCommand ShowUnavailableBooksCommand { get; set; }
+
+        public ShowAvailableBooksViewModel ShowAvailableBooksVM { get; set; }
+
+        public ShowUnavailableBooksViewModel ShowUnavailableBooksVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged(); 
+                }
+        }
+
+        public MainViewModel()
+        {
+            ShowAvailableBooksVM = new ShowAvailableBooksViewModel();
+            ShowUnavailableBooksVM = new ShowUnavailableBooksViewModel();
+            
+            CurrentView = ShowAvailableBooksVM;
+
+            ShowAvailableBooksCommand = new RelayCommand(o => CurrentView = ShowAvailableBooksVM);
+            ShowUnavailableBooksCommand = new RelayCommand(o => CurrentView = ShowUnavailableBooksVM);
+        }
+    }
+}
